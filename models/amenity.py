8ec62c75fd702a446f775a36861cc762cd7214a4
Andres Ortiz <3283@holbertonschool.com>
2021-12-21 09:06:04 -0800

    reversed relationships to tests previous cases

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
-from sqlalchemy import Column, Integer, String, Float
-from sqlalchemy.orm import relationship
+
 
 class Amenity(BaseModel):
-    __tablename__ = 'amenities'
-    name = Column(String(128), nullable=False)
+    name = ""
