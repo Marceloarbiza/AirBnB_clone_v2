1667caa91d1b7d1b5bc5bf401d96864f0f551792
Andres Ortiz <3283@holbertonschool.com>
2021-12-22 10:58:11 -0800

    Corrections

--- console.py
+++ console.py
@@ -144,9 +144,8 @@ class HBNBCommand(cmd.Cmd):
                         value = int(value)
                 setattr(new_instance, key, value)
         # ---------------------------------
-        storage.save()
+        new_instance.save()
         print(new_instance.id)
-        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
