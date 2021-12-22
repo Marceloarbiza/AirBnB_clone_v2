61cdc4194cc4275977d507ed6b116151f4a9eeb3
Marcelo <3338@holbertonschool.com>
2021-12-22 07:12:31 -0800

    basemodel

---
+++ models/statecpy.py
@@ -0,0 +1,29 @@
+""" State Module for HBNB project """
+from models.base_model import BaseModel
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from models.city import City, Base
+import os
+
+
+class State(BaseModel, Base):
+    """ State class """
+    __tablename__ = 'states'
+    # name = ""
+    name = Column(String(128), nullable=False)
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', backref='state',
+			      cascade='all, delete-orphan')
+    else:
+        @property
+        def cities(self):
+            """ storage """
+            from models import storage
+            ret_cities = []
+            all_cities = storage.all(City)
+            for c in all_cities.values():
+                if c.state_id == seld.id:
+                    ret_cities.append(c)
+            return ret_cities
