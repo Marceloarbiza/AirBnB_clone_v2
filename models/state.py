0813a9b04e532aa9e87cc46625e57a05233345f3
Marcelo <3338@holbertonschool.com>
2021-12-22 07:09:50 -0800

    state

--- models/state.py
+++ models/state.py
@@ -14,7 +14,8 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
 
     cities = relationship('City', backref='state',
-			  cascade='all, delete-orphan')
+                          cascade='all, delete-orphan')
+
     @property
     def cities(self):
         """ storage """
