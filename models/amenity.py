e4a5e72ca3bfba754d12896449ad4db22d449b42
Andres Ortiz <3283@holbertonschool.com>
2021-12-22 07:22:48 -0800

    Added relationships

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
-
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy.orm import relationship
 
 class Amenity(BaseModel):
-    name = ""
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship('Place' 'Amenity')
+    else:
+        name = ""
--- models/review.py
+++ models/review.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalquemy import Column, Integer, String, ForeignKey
 
 
 class Review(BaseModel):
     """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'reviews'
+        place_id = Column(String(60), nullable=False, ForeignKey('places.id'))
+        user_id = Column(String(60), nullable=False, ForeignKey('users.id'))
+        text = Column(String(1024), nullable=False)
+    else:
+	place_id = ""
+    	user_id = ""
+    	text = ""
