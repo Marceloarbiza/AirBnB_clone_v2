268e72296f7fce5db99cc4db6eefba70f2a6a21d
Andres Ortiz <3283@holbertonschool.com>
2021-12-22 07:30:18 -0800

    restored changes

--- models/user.py
+++ models/user.py
@@ -1,24 +1,11 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship, backref
+from models.base_model import BaseModel
 
 
 class User(BaseModel):
     """This class defines a user by various attributes"""
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
-        places = relationship('Place', cascade="all, delete-orphan",
-                              backref='user')
-        reviews = relationship('Review', cascade="all, delete-orphan",
-                               backref='user')
-    else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
