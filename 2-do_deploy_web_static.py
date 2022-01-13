27693ce2dd6733b51f1bbc0cf63abe9d6930bd4a
Marcelo <3338@holbertonschool.com>
2022-01-13 14:51:52 -0800

    2

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+Write a Fabric script that generates a .tgz archive
+from the contents of the web_static folder
+"""
+
+
+from fabric.api import run
+from fabric.api import local
+from fabric.api import get
+from fabric.api import put
+from fabric.api import env
+from datetime import datetime
+import os
+
+
+def do_pack():
+    """ do_pack """
+    complete_time = datetime.now()
+    string_time = complete_time.strftime("%Y%m%d%H%M%S")
+    tgz_name = string_time + '.tgz'
+    local("mkdir -p versions")
+    local("tar -cvzf versions/web_static_{} web_static".format(tgz_name))
+    path_tgz = 'versions/web_static_{}'.format(tgz_name)
+    if os.path.exists(path_tgz):
+        return path_tgz
+    else:
+        return None
+
+
+def do_deploy(archive_path):
+    """ do_deploy """
+    if os.path.exists(archive_path):
+        upload = put(archive_path, "/tmp")
+        ar_path_no_ext = archive_patch.split('.')[0]
+        ar_path_no_ext = archive_patch.split('/')[1]
+        ar_tmp = '/tmp/{}.tgz'.format(ar_path_no_ext)
+        run("tar -xvzf {} -C /data/web_static/releases/{}"
+            .format(ar_tmp, ar_path_no_ext))
+        run("rm {}".format(ar_tmp))
+        run("rm /data/web_static/current")
+        run("ln -s /data/web_static/releases/{} /data/web_static/current"
+            .format(ar_path_no_ext))
+        env.hosts = ['34.74.161.43', '35.185.60.222']
+    else:
+        return False
