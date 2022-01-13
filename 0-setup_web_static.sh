f9dca9463db56288ad4bacf1e0ea595361749135
Marcelo <3338@holbertonschool.com>
2022-01-13 11:17:24 -0800

    000

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,8 +5,6 @@ apt update
 apt install nginx -y
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
-chown -R ubuntu /data
-chgrp -R ubuntu /data
 echo "<html>
   <head>
   </head>
@@ -15,5 +13,7 @@ echo "<html>
   </body>
 </html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu /data
+chgrp -R ubuntu /data
 sed -i "/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/; }" /etc/nginx/sites-available/default
 service nginx restart
