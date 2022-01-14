06bef3949706e64ab8c6e69d3b41fe016e1f3b5b
Marcelo <3338@holbertonschool.com>
2022-01-13 17:03:20 -0800

    434343

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -12,7 +12,6 @@ from fabric.api import put
 from fabric.api import env
 from datetime import datetime
 import os
-from os.path import exists
 
 env.hosts = ['34.74.161.43', '35.185.60.222']
 
@@ -33,20 +32,20 @@ def do_pack():
 
 def do_deploy(archive_path):
     """ do_deploy """
-    if exists(archive_path) is False:
+    if os.path.exists(archive_path) is False:
         return False
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
+        file_tgz = archive_path.split("/")[-1]
+        f_no_ext = file_n.split(".")[0]
+        dir_rel = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('mkdir -p {}{}/'.format(dir_rel, f_no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_tgz, dir_rel, f_no_ext))
+        run('rm /tmp/{}'.format(file_tgz))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(dir_rel, f_no_ext))
+        run('rm -rf {}{}/web_static'.format(dir_rel, f_no_ext))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        run('ln -s {}{}/ /data/web_static/current'.format(dir_rel, f_no_ext))
         return True
     except Exception:
         return False
