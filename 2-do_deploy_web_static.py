1c735cfadb0a532194d0acbb55c82db89bca250f
Marcelo <3338@holbertonschool.com>
2022-01-13 17:06:41 -0800

    444sefini

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -35,17 +35,17 @@ def do_deploy(archive_path):
     if os.path.exists(archive_path) is False:
         return False
     try:
-        file_tgz = archive_path.split("/")[-1]
-        f_no_ext = file_n.split(".")[0]
-        dir_rel = "/data/web_static/releases/"
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(dir_rel, f_no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_tgz, dir_rel, f_no_ext))
-        run('rm /tmp/{}'.format(file_tgz))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(dir_rel, f_no_ext))
-        run('rm -rf {}{}/web_static'.format(dir_rel, f_no_ext))
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(dir_rel, f_no_ext))
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
     except Exception:
         return False
