e3d019fba84f344ed8c1b1f82b3ee223a32d5a27
Marcelo <3338@holbertonschool.com>
2022-01-29 08:07:42 -0800

    bm

--- models/base_model.py
+++ models/base_model.py
@@ -53,14 +53,15 @@ class BaseModel:
         """Convert instance into dict format"""
         dictionary = {}
         dictionary.update(self.__dict__)
-        if '_sa_instance_state' in dictionary:
-            del dictionary['_sa_instance_state']
         dictionary.update({'__class__':
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        # remove the key _sa_instance_state from the dictionary
-        # returned by this method only if this key exists
+        try:
+            if dictionary['_sa_instance_state']:
+                del dictionary['_sa_instance_state']
+        except Exception:
+            pass
         return dictionary
 
     def delete(self):
