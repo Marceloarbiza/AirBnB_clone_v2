c7abccfb01e0117a85ce58193a7f0c63d18cb1fe
Marcelo <3338@holbertonschool.com>
2022-01-29 09:42:18 -0800

    state

--- models/state.py
+++ models/state.py
@@ -1,28 +1,27 @@
+#!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
 from models.city import City
-import os
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class State(BaseModel, Base):
     """ State class """
+
+    """for sqlalchemy"""
     __tablename__ = 'states'
-    # name = ""
     name = Column(String(128), nullable=False)
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship('City', backref='state',
-                              cascade='all, delete-orphan')
-    else:
-        @property
-        def cities(self):
-            """ storage """
-            from models import storage
-            ret_cities = []
-            all_cities = storage.all(City)
-            for c in all_cities.values():
-                if c.state_id == self.id:
-                    ret_cities.append(c)
-            return ret_cities
+    cities = relationship("City", cascade="all, delete-orphan",
+                          backref="states")
+
+    @property
+    def cities(self):
+        """getter aall cities of State"""
+        from models import storage
+        all_cities = []
+        for c in list(storage.all(City).values()):
+            if c.state_id == self.id:
+                all_cities.append(c)
+        return all_cities
