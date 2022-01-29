ac85f1db9c4bf8f29122a6258a97bd7b0fdf7fec
Marcelo <3338@holbertonschool.com>
2022-01-29 12:56:36 -0800

    7plus

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,8 +10,8 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def states():
     """Returns a html listing the states"""
-    return render_template('7-states_list.html',
-                           states=storage.all('State').values())
+    states = storage.all('State').values()
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
