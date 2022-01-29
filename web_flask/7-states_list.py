53700bf173359809d11b400b1cc8cfbe40c8ba71
Marcelo <3338@holbertonschool.com>
2022-01-29 11:23:59 -0800

    77

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,9 +6,25 @@
 
 
 from flask import Flask, render_template
+from models import storage
 
 
 app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_():	
+    """ close storage """
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states():
+    """ states
+    all_states = storage.all('State')
+    """
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', all_states=states)
 
 
 if __name__ == "__main__":
