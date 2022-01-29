127b5f93caa7e8295aa927bda2e603a007b9173c
Marcelo <3338@holbertonschool.com>
2022-01-29 05:31:15 -0800

    1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -3,20 +3,20 @@
     /hbnb: display “HBNB”
 """
 
-
 from flask import Flask
-
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """ index """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """ return HBNB """
     return 'HBNB!'
 
 
