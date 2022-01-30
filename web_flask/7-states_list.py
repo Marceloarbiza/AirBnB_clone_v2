24262dc0715a9c0776fd32ca22b7501485fba587
Marcelo <3338@holbertonschool.com>
2022-01-30 07:06:22 -0800

    7788

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,19 +4,21 @@
 from models import storage
 from models.state import State
 from flask import Flask, render_template
+
+
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def display_states():
-    """Returns a html listing the states"""
-    states = storage.all('State')
-    return render_template('7-states_list.html', states=states)
+    """ Display the states"""
+    storage_states = storage.all('State')
+    return render_template('7-states_list.html', storage_states=states)
 
 
 @app.teardown_appcontext
 def teardown(self):
-    """remove the current SQLAlchemy Session"""
+    """ close storage """
     storage.close()
 
 
