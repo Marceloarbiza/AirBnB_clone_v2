0606b0ee3853d418730c18ac2eeb97dfe020ed04
Marcelo <3338@holbertonschool.com>
2022-01-30 06:46:31 -0800

    88

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" App Flask """
+
+from models import storage
+from models.state import State
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def display_states():
+    """Returns a html listing the states"""
+    states = storage.all('State')
+    cities = storage.all('City')
+    return render_template('7-states_list.html', states=states, cities=cities)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
