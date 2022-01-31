2aa9df860821829b6c66c61e09cfe1b74b68642e
Marcelo <3338@holbertonschool.com>
2022-01-30 17:32:10 -0800

    99

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -11,12 +11,12 @@ app = Flask(__name__)
 
 @app.route('/states', strict_slashes=False)
 @app.route('/states/<state_id>', strict_slashes=False)
-def states(id_state=None):
-    """ D """
-    storage_states = storage.all(State)
-    if id_state is not None:
-        id_state = 'State.' + state_id
-    return render_template('9-states.html', states=storage_states, id_state=id_state)
+def states(state_id=None):
+    """display a HTML page: (inside the tag BODY)"""
+    states = storage.all("State")
+    if state_id is not None:
+        state_id = 'State.' + state_id
+    return render_template('9-states.html', states=states, state_id=state_id)
 
 
 @app.teardown_appcontext
